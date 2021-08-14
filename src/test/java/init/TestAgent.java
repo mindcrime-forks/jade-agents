0a367e9426ef1ae04bc84b6b2a52a25db474ae84
hc <cedric.herpson@lip6.fr>
2021-08-15 00:03:19 +0200

    Fixed missing resources for tools

--- src/test/java/init/TestAgent.java
+++ src/test/java/init/TestAgent.java
@@ -1,3 +1,5 @@
+package init;
+
 /*****************************************************************
 Jade - Java Agent DEvelopment Framework is a framework to develop
 multi-agent systems in compliance with the FIPA specifications.
